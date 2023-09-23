b681fbf69ea94e23b060763b2dccebde7dfdfedd
yhoungdev <yhoungdev@gmail.com>
2023-09-23 04:52:57 +0100

    chore: (UI) included header avatar

--- src/app/_components/molecules/header/dashboardHeader.tsx
+++ src/app/_components/molecules/header/dashboardHeader.tsx
@@ -1,20 +1,33 @@
 import { header_nav } from '@/app/_utils/navs';
-import { Box, ListItem, UnorderedList, flexbox } from '@chakra-ui/react';
+import {
+  Box,
+  ListItem,
+  Avatar,
+  UnorderedList,
+  flexbox,
+} from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
 
 const DashboardHeader = () => {
   return (
-    <Box display={'flex'}>
+    <Box
+      display={'flex'}
+      alignItems={'center'}
+      justifyContent={'space-between'}
+    >
       <Box>
-        header
-        <UnorderedList>
+        <UnorderedList display={'flex'} listStyleType={'none'} gap={'3em'}>
           {header_nav.map((_, index) => (
             <Link href={''} key={index}>
               <ListItem>{_.title}</ListItem>
             </Link>
           ))}
         </UnorderedList>
+      </Box>
+
+      <Box>
+        <Avatar name={'avatar'} />
       </Box>
     </Box>
   );
